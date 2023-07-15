68fe46018b0e61117c12b2f6ef88a509b7503109
Riku Rauhala <riku@rauhala.io>
2023-07-15 22:10:00 +0300

    Implement basic routing in the frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,21 @@
-import Main from './components/Main';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
-const App = (): JSX.Element => (
-  <Main />
-);
+const Home = () => <h1>Home Page</h1>;
+const Register = () => <h1>Register</h1>;
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="/">home</Link>
+        <Link to="/register">register</Link>
+      </div>
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Register />} path="/register" />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
