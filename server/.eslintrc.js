c6e91d15fabd4073efc8884aa65bdc19686e6ab8
Riku Rauhala <riku@rauhala.io>
2023-07-16 20:15:55 +0300

    Fix server eslintrc

---
+++ server/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  plugins: ['@typescript-eslint'],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/restrict-plus-operands': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/semi': ['error'],
+    'eol-last': ['error', 'always'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'max-len': ['error', { code: 120, tabWidth: 2 }],
+    'no-case-declarations': 'off',
+    'no-multi-spaces': 'error',
+    'no-trailing-spaces': 'error',
+    'no-useless-escape': 'off',
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+  },
+  ignorePatterns: ['build', 'web'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+};
