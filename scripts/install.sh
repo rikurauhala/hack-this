1c568638891f0a9ea76a6a6afb72ad6429c219fc
Riku Rauhala <riku@rauhala.io>
2023-07-25 20:05:20 +0300

    Create an installation script

---
+++ scripts/install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Get the source code
+git clone git@github.com:rikurauhala/hack-this.git
+
+# Install backend
+echo "Installing backend..."
+cd ./hack-this/server
+npm install
+echo "Backend installed"
+
+# Initialize the database
+echo "Initializing database..."
+cd ./data
+touch database.db
+sqlite3 database.db < init.sql
+echo "Database initialized"
+
+# Create the .env file
+echo "Configuring..."
+cd ..
+touch .env
+current_dir=$(pwd)
+echo DATABASE_URL="$current_dir/data/database.db" >> .env
+random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 25 | head -n 1)
+echo SECRET=$random_string >> .env
+
+# Install frontend
+echo "Installing frontend..."
+cd ../web
+npm install
+echo "Frontend installed"
+
+# Run the application or finish installation
+echo "Do you want to run the application now? (y/n): "
+read choice
+while true; do
+    if [[ "$choice" = "y" ]]; then
+        npm start
+        cd ../server
+        npm run dev
+        break
+    elif [[ "$choice" = "n" ]]; then
+        echo "Installation finished"
+        break
+    else
+        echo "Invalid input. Please enter either 'y' or 'n': "
+        read choice
+    fi
+done
