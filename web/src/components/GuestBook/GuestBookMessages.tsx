9efd5c70911feba66349bc6576a067b1a5cf46ad
Riku Rauhala <riku@rauhala.io>
2023-08-05 11:38:07 +0300

    Fix the guestbook if there are no messages yet

--- web/src/components/GuestBook/GuestBookMessages.tsx
+++ web/src/components/GuestBook/GuestBookMessages.tsx
@@ -1,5 +1,5 @@
-import { colorText } from '../../theme';
 import { Message } from '../../types';
+import Paragraph from '../common/Paragraph';
 import Subtitle from '../common/Subtitle';
 import GuestBookMessage from './GuestBookMessage';
 
@@ -7,28 +7,22 @@ interface GuestBookMessagesProps {
   messages: Message[];
 }
 
-const GuestBookMessages = ({ messages }: GuestBookMessagesProps): JSX.Element => {
-  if (!messages) {
-    return (
-      <div className={`${colorText}`}>
-        No message yet
-      </div>
-    );
-  }
-
-  return (
-    <div className="mt-8">
-      <Subtitle text="Messages" />
-      <ul>
-        {messages.map((message) => (
+const GuestBookMessages = ({ messages }: GuestBookMessagesProps): JSX.Element => (
+  <div className="mt-8">
+    <Subtitle text="Messages" />
+    <ul>
+      {messages.length === 0 ? (
+        <Paragraph text="No messages yet!" />
+      ) : (
+        messages.map((message) => (
           <GuestBookMessage
             key={message.messageId}
             message={message}
           />
-        ))}
-      </ul>
-    </div>
-  );
-};
+        ))
+      )}
+    </ul>
+  </div>
+);
 
 export default GuestBookMessages;
