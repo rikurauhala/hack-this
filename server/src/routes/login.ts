4f742f0f73f71478daaf5d2c300a4b444d716698
Riku Rauhala <riku@rauhala.io>
2023-08-12 11:53:39 +0300

    Remove try-catch

--- server/src/routes/login.ts
+++ server/src/routes/login.ts
@@ -8,34 +8,29 @@ import { User } from '../types';
 const loginRouter = Router();
 
 loginRouter.post('/', async (request: Request, response: Response) => {
-  try {
-    const username = request.body.username as string;
-    const password = request.body.password as string;
+  const username = request.body.username as string;
+  const password = request.body.password as string;
 
-    const user: User | null = await db.getUser(username, password);
+  const user: User | null = await db.getUser(username, password);
 
-    if (!user) {
-      return response.status(401).json({ message: 'Invalid username or password' });
-    }
-
-    const userForToken = {
-      username: user.username,
-      id: user.id,
-    };
-
-    const token = jwt.sign(userForToken, SECRET);
-
-    return response
-      .status(200)
-      .json({
-        id: user.id,
-        token: token,
-        username: user.username
-      });
-  } catch (error) {
-    console.error('Error during login:', error);
-    return response.status(500).json({ message: 'Error during login' });
+  if (!user) {
+    return response.status(401).json({ message: 'Invalid username or password' });
   }
+
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  };
+
+  const token = jwt.sign(userForToken, SECRET);
+
+  const userJson = {
+    id: user.id,
+    token: token,
+    username: user.username
+  };
+
+  return response.status(200).json(userJson);
 });
 
 export default loginRouter;
--- server/src/routes/register.ts
+++ server/src/routes/register.ts
@@ -6,28 +6,24 @@ import { validatePassword, validateUsername } from '../utils/validators';
 const registerRouter = Router();
 
 registerRouter.post('/', async (request: Request, response: Response) => {
-  try {
-    const username = request.body.username as string;
-    const usernameValid = validateUsername(username);
-    if (!usernameValid.valid) {
-      return response.status(400).json({ message: usernameValid.errorMessage });
-    }
-    if (await db.usernameIsTaken(username)) {
-      return response.status(400).json({ message: 'This username is already taken' });
-    }
-    const password = request.body.password as string;
-    const passwordValid = validatePassword(password);
-    if (!passwordValid.valid) {
-      return response.status(400).json({ message: passwordValid.errorMessage });
-    }
-
-    await db.registerUser(username, password);
-    console.log('Registration successful');
-    return response.status(201).json({ message: 'Registration successful' });
-  } catch (error) {
-    console.error('Error during registration:', error);
-    return response.status(500).json({ message: 'Error during registration' });
+  const username = request.body.username as string;
+  const usernameValid = validateUsername(username);
+  if (!usernameValid.valid) {
+    return response.status(400).json({ message: usernameValid.errorMessage });
   }
+  if (await db.usernameIsTaken(username)) {
+    return response.status(400).json({ message: 'This username is already taken' });
+  }
+
+  const password = request.body.password as string;
+  const passwordValid = validatePassword(password);
+  if (!passwordValid.valid) {
+    return response.status(400).json({ message: passwordValid.errorMessage });
+  }
+
+  await db.registerUser(username, password);
+  console.log('Registration successful');
+  return response.status(201).json({ message: 'Registration successful' });
 });
 
 export default registerRouter;
