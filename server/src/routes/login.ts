dd123f519ca19a86f612745ddaeceed3e468ded3
Riku Rauhala <riku@rauhala.io>
2023-08-20 20:46:50 +0300

    Add rate limiting to the login router

--- server/src/routes/login.ts
+++ server/src/routes/login.ts
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
 import Router from 'express-promise-router';
+import rateLimit from 'express-rate-limit';
 import jwt from 'jsonwebtoken';
 import { SECRET } from '../utils/config';
 import { db } from '../db';
@@ -8,7 +9,13 @@ import { User } from '../types';
 
 const loginRouter = Router();
 
-loginRouter.post('/', async (request: Request, response: Response) => {
+const loginRateLimit = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: { message: 'Too many requests, please try again later.' }
+});
+
+loginRouter.post('/', loginRateLimit, async (request: Request, response: Response) => {
   const username = request.body.username as string;
   const password = request.body.password as string;
 
