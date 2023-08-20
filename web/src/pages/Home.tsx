49b03a71bda5003cf8a98f2ea837d99baf64c415
Riku Rauhala <riku@rauhala.io>
2023-08-20 16:21:19 +0300

    Remove lorem ipsum from the home page

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -9,16 +9,6 @@ const Home = (): JSX.Element => {
     <div className="container py-4">
       <Title text="Home" />
       <Paragraph text={'Welcome to the Hack This web application security demo.'} />
-      <Paragraph
-        text={`
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus facilisis odio, nec blandit mauris
-          finibus eu. Maecenas pharetra, felis ac venenatis hendrerit, nisi nunc eleifend tortor, a vehicula elit
-          turpis vitae eros. Nullam laoreet nisi et erat facilisis, a venenatis urna auctor. Sed auctor justo sit amet
-          mauris fermentum, nec congue est scelerisque. Duis eu ex metus. Integer facilisis urna vel tempor iaculis.
-          Ut vitae odio vel elit fermentum porttitor in eget nulla. Nam commodo velit vel ultrices luctus. Suspendisse
-          potenti. Nulla facilisi.
-        `}
-      />
     </div>
   );
 };
